d2ab9c7e9c413a541df1b5004ed888cd2b825efd
Ingmar Steen <iksteen@gmail.com>
2013-05-16 23:48:07 +0200

    pep8 jaspyx.py

--- jaspyx.py
+++ jaspyx.py
@@ -5,11 +5,12 @@ from jaspyx.visitor import DefaultVisitor
 
 
 if __name__ == '__main__':
-  import sys
-  if len(sys.argv) < 2:
-    print >>sys.stderr, 'Syntax: %s <input>' % sys.argv[0]
-    sys.exit(1)
-  c = ast.parse(open(sys.argv[1]).read(), sys.argv[1])
-  v = DefaultVisitor()
-  v.visit(c)
-  print str(v.module)
+    import sys
+
+    if len(sys.argv) < 2:
+        print >> sys.stderr, 'Syntax: %s <input>' % sys.argv[0]
+        sys.exit(1)
+    c = ast.parse(open(sys.argv[1]).read(), sys.argv[1])
+    v = DefaultVisitor()
+    v.visit(c)
+    print str(v.module)
