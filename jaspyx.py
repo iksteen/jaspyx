b27b7bc01b16cd4af1062d2371c2d9e0227102f9
Ingmar Steen <iksteen@gmail.com>
2013-05-16 23:48:05 +0200

    Add support for retrieving slices

--- jaspyx.py
+++ jaspyx.py
@@ -386,6 +386,26 @@ class JaspyxVisitor(ast.NodeVisitor):
   def visit_RShift(self, node):
     self.output('>>>')
 
+  def visit_Subscript(self, node):
+    self.visit(node.value)
+    self.visit(node.slice)
+
+  def visit_Index(self, node):
+    self.output('[')
+    self.visit(node.value)
+    self.output(']')
+
+  def visit_Slice(self, node):
+    self.output('.slice(')
+    if node.lower is None:
+      self.output('0')
+    else:
+      self.visit(node.lower)
+    if node.upper is not None:
+      self.output(', ')
+      self.visit(node.upper)
+    self.output(')')
+
   # Comparison:
   def visit_Compare(self, node):
     if len(node.ops) > 1:
