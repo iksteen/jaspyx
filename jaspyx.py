61afbcc08fcbfcf25ca452f0b8a3c0942189f4e6
Ingmar Steen <iksteen@gmail.com>
2013-05-16 23:48:04 +0200

    Initial commit

---
+++ jaspyx.py
@@ -0,0 +1,493 @@
+#! /usr/bin/env python
+
+import ast
+import _ast
+import json
+
+
+BUILTINS = {
+  'undefined': 'undefined',
+  'None': 'null',
+  'window': 'window',
+  'True': 'true',
+  'False': 'false',
+  'alert': 'alert',
+  'Math': 'Math',
+  'self': 'this',
+}
+
+
+class Scope(object):
+  def __init__(self, parent=None):
+    self.parent = parent
+    self.declarations = set()
+    self.references = set()
+    self.globals = set()
+
+  def declare(self, name):
+    self.declarations.add(name)
+
+  def is_declared(self, name):
+    if name in self.declarations:
+      return True
+    elif self.parent is not None:
+      return self.parent.is_declared(name)
+    else:
+      return False
+
+  def reference(self, name):
+    self.references.add(name)
+
+  def declare_global(self, name):
+    self.globals.add(name)
+
+  def is_global(self, name):
+    return name in self.globals
+
+class PartialStatement(object):
+  def __init__(self, content):
+    self.content = content
+
+  def __str__(self):
+    return self.content
+
+class ContinuedStatement(PartialStatement):
+  def __str__(self):
+    return self.content
+
+
+class Statement(ContinuedStatement):
+  def __str__(self):
+    return '%s;\n' % self.content
+
+
+class Block(object):
+  def __init__(self, parent, cr=True):
+    if parent:
+      self.indent = parent.indent + 2
+      self.scope = parent.scope
+    else:
+      self.indent = 0
+      self.scope = Scope()
+    self.body = []
+    self.cr = cr
+
+  def add(self, part):
+    self.body.append(part)
+
+  def __str__(self):
+    return '{\n%s%s}%s' % (
+      ''.join([str(s) for s in self.body]),
+      ' ' * self.indent,
+      self.cr and '\n' or '',
+    )
+
+
+class InlineFunction(Block):
+  def __init__(self, parent, name, arg_names=[]):
+    super(InlineFunction, self).__init__(parent, cr=False)
+    self.scope = Scope(self.scope)
+    self.name = name
+    self.arg_names = arg_names
+    for arg_name in self.arg_names:
+      self.scope.declare(arg_name)
+
+  def __str__(self):
+    declare_vars = []
+    for reference in self.scope.references:
+      if not self.scope.is_declared(reference):
+        self.scope.declare(reference)
+        declare_vars.append(reference)
+
+    if declare_vars:
+      indent = ' ' * (self.indent + 2)
+      stmt = Statement('%svar %s' % (indent, ', '.join(declare_vars)))
+      self.body.insert(0, stmt)
+
+    return 'function%s(%s) %s' % (
+      self.name and ' %s' % self.name or '',
+      ', '.join(self.arg_names),
+      super(InlineFunction, self).__str__()
+    )
+
+
+class Function(InlineFunction):
+  def __init__(self, parent, name, arg_names=[]):
+    super(Function, self).__init__(parent, name, arg_names)
+    self.cr = False
+
+  def __str__(self):
+    return '%s%s\n' % (
+      ' ' * self.indent,
+      super(Function, self).__str__(),
+    )
+
+
+class Module(InlineFunction):
+  def __init__(self):
+    super(Module, self).__init__(None, '')
+    for builtin in BUILTINS.values():
+      self.scope.declare(builtin)
+
+  def __str__(self):
+    return '(%s).call(this);' % super(Module, self).__str__()
+
+
+class JaspyxVisitor(ast.NodeVisitor):
+  def __init__(self):
+    self.stack = []
+    self.module = None
+    self.do_indent = False
+    self.stmt_buf = []
+
+  def output(self, s):
+    if self.do_indent and not self.stmt_buf:
+      self.stmt_buf.append(' ' * (self.stack[-1].indent + 2))
+      self.do_indent = False
+    self.stmt_buf.append(s)
+
+  def flush(self, stmt_class=Statement):
+    if self.stmt_buf:
+      self.stack[-1].add(stmt_class(''.join(self.stmt_buf)))
+      self.stmt_buf = []
+
+  def block(self, nodes):
+    for node in nodes:
+      self.do_indent = True
+      self.visit(node)
+      self.flush()
+
+  # Scoped operations:
+  def visit_Module(self, node):
+    self.module = Module()
+    self.stack.append(self.module)
+    self.block(node.body)
+    self.stack.pop()
+
+  def visit_FunctionDef(self, node):
+    self.stack[-1].scope.declare(node.name)
+
+    args = [arg.id for arg in node.args.args]
+    if node.args.vararg is not None:
+      raise Exception('*args not supported')
+    if node.args.kwarg is not None:
+      raise Exception('**kwargs not supported')
+
+    func = Function(self.stack[-1], node.name, args)
+    self.stack.append(func)
+
+    def_args = node.args.defaults
+    for arg_name, arg_val in zip(args[-len(def_args):], def_args):
+      self.block([
+        _ast.If(
+          _ast.Compare(
+            _ast.Call(
+              _ast.Name('type', _ast.Load()),
+              [_ast.Str(arg_name),],
+              None,
+              None,
+              None,
+            ),
+            [_ast.Eq(),],
+            [_ast.Str('undefined'),],
+          ),
+          [
+            _ast.Assign(
+              [_ast.Name(arg_name, _ast.Store())],
+              arg_val
+              ),
+          ],
+          [],
+        )
+      ])
+
+    self.block(node.body)
+    self.stack[-2].add(self.stack.pop())
+    
+
+  def visit_Lambda(self, node):
+    self.flush(PartialStatement)
+
+    args = [arg.id for arg in node.args.args]
+
+    func = InlineFunction(self.stack[-1], '', args)
+    self.stack.append(func)
+    self.block([_ast.Return(node.body)])
+    self.stack[-2].add(self.stack.pop())
+
+  # Print
+  def visit_Print(self, node):
+    self.output('window.console.log(')
+    first = True
+    for value in node.values:
+      if not first:
+        self.output(', ')
+      else:
+        first = False
+      self.visit(value)
+    self.output(')')
+
+  # Literal operations
+  def visit_Num(self, node):
+    self.output(json.dumps(node.n))
+
+  def visit_Str(self, node):
+    self.output(json.dumps(node.s))
+
+  def visit_List(self, node):
+    first = True
+    for elt in node.elts:
+      if not first:
+        self.output(',')
+      else:
+        first = False
+      self.visit(elt)
+
+  # Variable operations:
+  def visit_Global(self, node):
+    for name in node.names:
+      self.stack[-1].scope.declare_global(name)
+
+  def visit_Name(self, node):
+    if self.stack[-1].scope.is_global(node.id):
+      name = 'window.%s' % node.id
+    else:
+      name = BUILTINS.get(node.id, node.id)
+      self.stack[-1].scope.reference(name)
+    self.output(name)
+
+  def visit_Assign(self, node):
+    for target in node.targets:
+      self.visit(target)
+    self.output(' = ')
+    self.visit(node.value)
+
+  def visit_Attribute(self, node):
+    self.visit(node.value)
+    self.output('.%s' % node.attr)
+
+  # Control flow operations:
+  def visit_Expr(self, node):
+    self.visit(node.value)
+
+  def visit_Pass(self, node):
+    pass
+
+  def visit_Return(self, node):
+    self.output('return ')
+    self.visit(node.value)
+
+  def builtin_JS(self, arg):
+    if not isinstance(arg, _ast.Str):
+      raise Exception('JS() expects a string argument')
+    self.output(arg.s)
+
+  def builtin_type(self, arg):
+    self.output('typeof ')
+    self.visit(arg)
+
+  def visit_Call(self, node):
+    if node.keywords:
+      raise Exception('keyword arguments are not supported')
+    if node.starargs is not None:
+      raise Exception('starargs is not supported')
+    if node.kwargs is not None:
+      raise Exception('kwargs is not supported')
+
+    if isinstance(node.func, _ast.Name):
+      builtin = getattr(self, 'builtin_%s' % node.func.id, None)
+      if builtin is not None:
+        return builtin(*node.args)
+
+    self.visit(node.func)
+    self.output('(')
+    first = True
+    for arg in node.args:
+      if not first:
+        self.output(', ')
+      else:
+        first = False
+      self.visit(arg)
+    self.output(')')
+
+  def visit_If(self, node):
+    self.output('if(')
+    self.visit(node.test)
+    self.output(')')
+    self.flush(PartialStatement)
+
+    self.stack.append(Block(self.stack[-1], cr=not node.orelse))
+    self.block(node.body)
+    self.stack[-2].add(self.stack.pop())
+
+    if node.orelse:
+      self.output('else')
+      self.flush(PartialStatement)
+      self.stack.append(Block(self.stack[-1], cr=True))
+      self.block(node.orelse)
+      self.stack[-2].add(self.stack.pop())
+
+  # Operators:
+  def visit_Add(self, node):
+    self.output('+')
+
+  def visit_Sub(self, node):
+    self.output('-')
+
+  def visit_Mult(self, node):
+    self.output('*')
+
+  def visit_Div(self, node):
+    self.output('/')
+
+  def visit_Mod(self, node):
+    self.output('%')
+
+  def visit_BitAnd(self, node):
+    self.output('&')
+
+  def visit_BitOr(self, node):
+    self.output('|')
+
+  def visit_Eq(self, node):
+    self.output('==')
+  
+  def visit_NotEq(self, node):
+    self.output('!=')
+  
+  def visit_Lt(self, node):
+    self.output('<')
+
+  def visit_LtE(self, node):
+    self.output('<=')
+  
+  def visit_Gt(self, node):
+    self.output('>')
+
+  def visit_GtE(self, node):
+    self.output('>=')
+
+  def visit_And(self, node):
+    self.output('&&')
+
+  def visit_Or(self, node):
+    self.output('||')
+
+  def visit_Is(self, node):
+    self.output('===')
+
+  def visit_IsNot(self, node):
+    self.output('!==')
+
+  # Comparison:
+  def visit_Compare(self, node):
+    if len(node.ops) > 1:
+      self.output('(')
+    first = True
+    left = node.left
+    for op, comparator in zip(node.ops, node.comparators):
+      if not first:
+        self.output(' && ')
+      else:
+        first = False
+      self.output('(')
+      self.visit(left)
+      self.output(' ')
+      self.visit(op)
+      self.output(' ')
+      self.visit(comparator)
+      self.output(')')
+      left = comparator
+    if len(node.ops) > 1:
+      self.output(')')
+
+  # Augmented assign operations:
+  def visit_AugAssign(self, node):
+    attr = getattr(self, 'visit_AugAssign_%s' % node.op.__class__.__name__, None)
+    if attr is None:
+      # Rewrite the expression as an assignment using a BinOp
+      self.visit(_ast.Assign(
+        [node.target],
+        _ast.BinOp(
+          _ast.Name(node.target.id, _ast.Load()),
+          node.op,
+          node.value
+        )
+      ))
+    else:
+      attr(node.target, node.op, node.value)
+
+  def visit_AugAssign__op(self, target, op, value):
+    self.visit(_ast.Name(target.id, _ast.Load()))
+    self.output(' ')
+    self.visit(op)
+    self.output('= ')
+    self.visit(value)
+  visit_AugAssign_Add = visit_AugAssign__op
+  visit_AugAssign_Sub = visit_AugAssign__op
+  visit_AugAssign_Mult = visit_AugAssign__op
+  visit_AugAssign_Div = visit_AugAssign__op
+
+  # Binary operations:
+  def visit_BinOp(self, node):
+    attr = getattr(self, 'visit_BinOp_%s' % node.op.__class__.__name__, None)
+    if attr is None:
+      self.output('(')
+      self.visit(node.left)
+      self.output(' ')
+      self.visit(node.op)
+      self.output(' ')
+      self.visit(node.right)
+      self.output(')')
+    else:
+      self.output('(')
+      attr(node.op, node.left, node.right)
+      self.output(')')
+
+  def visit_BinOp_Pow(self, node, left, right):
+    self.output('Math.pow(')
+    self.visit(left)
+    self.output(', ')
+    self.visit(right)
+    self.output(')')
+
+  def visit_BinOp_FloorDiv(self, node, left, right):
+    self.output('Math.floor(')
+    self.visit(left)
+    self.output(' / ')
+    self.visit(right)
+    self.output(')')
+
+  # Boolean operator:
+  def visit_BoolOp(self, node):
+    self.output('(')
+    first = True
+    for value in node.values:
+      if not first:
+        self.output(' ')
+        self.visit(node.op)
+        self.output(' ')
+      else:
+        first = False
+      self.visit(value)
+    self.output(')')
+
+  # Error handler:
+  def generic_visit(self, node):
+    import sys
+    print >>sys.stderr, node
+    print >>sys.stderr, 'Fields:'
+    for field in node._fields:
+      print >>sys.stderr, ' - %s: %s' % (field, getattr(node, field))
+    raise Exception('Unsupported AST node %s' % node)
+
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) < 2:
+    print >>sys.stderr, 'Syntax: %s <input>' % sys.argv[0]
+    sys.exit(1)
+  c = ast.parse(open(sys.argv[1]).read(), sys.argv[1])
+  v = JaspyxVisitor()
+  v.visit(c)
+  print str(v.module)
