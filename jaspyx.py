5b477968e35d0cd3aa71440f58e7258e291645c1
Ingmar Steen <iksteen@gmail.com>
2013-05-16 23:48:07 +0200

    Move Scope class to separate module in jaspyx package.

--- jaspyx.py
+++ jaspyx.py
@@ -5,33 +5,7 @@ import _ast
 import json
 
 from jaspyx.builtins import BUILTINS
-
-class Scope(object):
-  def __init__(self, parent=None):
-    self.parent = parent
-    self.declarations = set()
-    self.references = set()
-    self.globals = set()
-
-  def declare(self, name):
-    self.declarations.add(name)
-
-  def is_declared(self, name):
-    if name in self.declarations:
-      return True
-    elif self.parent is not None:
-      return self.parent.is_declared(name)
-    else:
-      return False
-
-  def reference(self, name):
-    self.references.add(name)
-
-  def declare_global(self, name):
-    self.globals.add(name)
-
-  def is_global(self, name):
-    return name in self.globals
+from jaspyx.scope import Scope
 
 
 class Context(object):
