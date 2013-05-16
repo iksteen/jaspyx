76993e30ee2d08f5d979df87ce0759677545dffb
Ingmar Steen <iksteen@gmail.com>
2013-05-16 23:48:05 +0200

    Add support for slice assignment

--- jaspyx.py
+++ jaspyx.py
@@ -254,7 +254,43 @@ class JaspyxVisitor(ast.NodeVisitor):
       self.stack[-1].scope.reference(name)
     self.output(name)
 
+  def visit_Assign_Slice(self, target, value):
+    if target.slice.lower is None:
+      lower = _ast.Num(0)
+    else:
+      lower = target.slice.lower
+
+    if target.slice.upper is None:
+      upper = _ast.Attribute(
+        target.value,
+        'length',
+        _ast.Load()
+      )
+    else:
+      upper = target.slice.upper
+
+    self.output('Array.prototype.splice.apply(')
+    self.visit(target.value)
+    self.output(', [')
+    self.visit(lower)
+    self.output(', ')
+    self.visit(upper)
+    self.output(' - ')
+    self.visit(lower)
+    self.output('].concat(')
+    self.visit(value)
+    self.output('))')
+
   def visit_Assign(self, node):
+    # Check for slice assignment
+    for target in node.targets:
+      if isinstance(target, _ast.Subscript) and \
+          isinstance(target.slice, _ast.Slice):
+        if len(node.targets) > 1:
+          raise Exception('Slice assignment only supported when assignment has a single target')
+        self.visit_Assign_Slice(target, node.value)
+        return
+
     for target in node.targets:
       self.visit(target)
     self.output(' = ')
