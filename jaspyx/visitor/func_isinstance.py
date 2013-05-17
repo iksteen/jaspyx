6f0acd22f912324f7bfb3b1a7eba6b61a5024642
Ingmar Steen <iksteen@gmail.com>
2013-05-17 22:14:31 +0200

    Surround instanceof with braces.

--- jaspyx/visitor/func_isinstance.py
+++ jaspyx/visitor/func_isinstance.py
@@ -3,6 +3,8 @@ from jaspyx.visitor import BaseVisitor
 
 class FuncIsinstance(BaseVisitor):
     def func_isinstance(self, obj, type_):
+        self.output('(')
         self.visit(obj)
         self.output(' instanceof ')
         self.visit(type_)
+        self.output(')')
