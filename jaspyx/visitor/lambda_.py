13b82b10dd5abe898525ef2f8664d29ead15d0bc
Ingmar Steen <iksteen@gmail.com>
2013-05-17 22:12:24 +0200

    Implement lambda using new inline function support.
    As a bonus, lambda functions now get default and vararg support.

--- jaspyx/visitor/lambda_.py
+++ jaspyx/visitor/lambda_.py
@@ -1,11 +1,12 @@
 import _ast
-from jaspyx.context.function import FunctionContext
 from jaspyx.visitor import BaseVisitor
 
 
 class Lambda(BaseVisitor):
     def visit_Lambda(self, node):
-        args = [arg.id for arg in node.args.args]
-
-        func = FunctionContext(self.stack[-1], '', args)
-        self.block([_ast.Return(node.body)], context=func)
+        self.visit(_ast.FunctionDef(
+            '',
+            node.args,
+            [_ast.Return(node.body)],
+            []
+        ))
