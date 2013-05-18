adab630e1a1e5e86ce5ab7389d578bf1f3a7d48c
Ingmar Steen <iksteen@gmail.com>
2013-05-18 18:01:57 +0200

    Improved ast_load, added ast_store.
    ast_store calls ast_load and changes the context of the top node to ast.Store()

--- jaspyx/ast_util.py
+++ jaspyx/ast_util.py
@@ -1,11 +1,19 @@
 import _ast
 
 
-def ast_load(path):
-    pieces = path.split('.')
+def ast_load(*args):
+    pieces = []
+    for arg in args:
+        pieces += arg.split('.')
     obj = _ast.Name(pieces[0], _ast.Load())
     for piece in pieces[1:]:
         obj = _ast.Attribute(obj, piece, _ast.Load())
+    return obj
+
+
+def ast_store(*args):
+    obj = ast_load(*args)
+    obj.ctx = _ast.Store()
     return obj
 
 
