b27af083227d2b4ded8496b254abce52791312ee
Ingmar Steen <iksteen@gmail.com>
2013-05-19 13:46:09 +0200

    Surround IfExp with braces.

--- jaspyx/visitor/ifexp.py
+++ jaspyx/visitor/ifexp.py
@@ -3,8 +3,10 @@ from jaspyx.visitor import BaseVisitor
 
 class IfExp(BaseVisitor):
     def visit_IfExp(self, node):
+        self.output('(')
         self.visit(node.test)
         self.output(' ? ')
         self.visit(node.body)
         self.output(' : ')
         self.visit(node.orelse)
+        self.output(')')
