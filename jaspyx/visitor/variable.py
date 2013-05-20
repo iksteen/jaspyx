31374fa73d511c7d7b1a27ed0fd36b902c901dd0
Ingmar Steen <iksteen@gmail.com>
2013-05-20 12:32:46 +0200

    Properly declare and fall back to globals.

--- jaspyx/visitor/variable.py
+++ jaspyx/visitor/variable.py
@@ -6,10 +6,12 @@ from jaspyx.visitor import BaseVisitor
 class Variable(BaseVisitor):
     def visit_Name(self, node):
         scope = self.stack[-1].scope
+        global_scope = scope.get_global_scope()
 
         if isinstance(node.ctx, _ast.Store):
             if scope.is_global(node.id):
-                self.output(scope.get_global_scope().prefixed(node.id))
+                global_scope.declare(node.id)
+                self.output(global_scope.prefixed(node.id))
             else:
                 scope.declare(node.id)
                 self.output(scope.prefixed(node.id))
@@ -21,6 +23,6 @@ class Variable(BaseVisitor):
                 if var_scope is not None:
                     self.output(var_scope.prefixed(node.id))
                 else:
-                    self.output(BUILTINS.get(node.id, node.id))
+                    self.output(BUILTINS.get(node.id, global_scope.prefixed(node.id)))
         else:
             raise NotImplementedError('name lookup not implemented for context %s' % node.ctx.__class__.__name__)
