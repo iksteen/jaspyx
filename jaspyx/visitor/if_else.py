faa21ec406a9e869a43ad941cc60e9e64c3ae543
Ingmar Steen <iksteen@gmail.com>
2013-05-26 01:04:20 +0200

    Small output cleanup for elif construction.

--- jaspyx/visitor/if_else.py
+++ jaspyx/visitor/if_else.py
@@ -1,10 +1,12 @@
+import _ast
 from jaspyx.context.block import BlockContext
 from jaspyx.visitor import BaseVisitor
 
 
 class IfElse(BaseVisitor):
-    def visit_If(self, node):
-        self.indent()
+    def visit_If(self, node, skip_indent=False):
+        if not skip_indent:
+            self.indent()
         self.output('if(')
         self.visit(node.test)
         self.output(') ')
@@ -13,6 +15,10 @@ class IfElse(BaseVisitor):
 
         if node.orelse:
             self.output(' else ')
-            self.block(node.orelse, context=BlockContext(self.stack[-1]))
-
-        self.output('\n')
+            if len(node.orelse) == 1 and isinstance(node.orelse[0], _ast.If):
+                self.visit_If(node.orelse[0], True)
+            else:
+                self.block(node.orelse, context=BlockContext(self.stack[-1]))
+                self.output('\n')
+        else:
+            self.output('\n')
