543533ede3299d2e22d790cfaebb50f58dd299e8
Ingmar Steen <iksteen@gmail.com>
2013-05-26 13:57:10 +0200

    Fixed BoolOp.

--- jaspyx/visitor/boolop.py
+++ jaspyx/visitor/boolop.py
@@ -3,8 +3,8 @@ from jaspyx.visitor import BaseVisitor
 
 class BoolOp(BaseVisitor):
     def visit_BoolOp(self, node):
-        attr = getattr(self, 'BinOp_%s' % node.op.__class__.__name__, None)
-        attr(node.left, node.right)
+        attr = getattr(self, 'BoolOp_%s' % node.op.__class__.__name__)
+        attr(node.values)
 
     def BoolOp_And(self, values):
         self.group(values, infix=' && ')
