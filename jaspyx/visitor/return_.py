c517eec14c9034d0e8709878ee47f48d084fe44e
Ingmar Steen <iksteen@gmail.com>
2013-08-06 16:34:19 +0200

    Fix return without value.

--- jaspyx/visitor/return_.py
+++ jaspyx/visitor/return_.py
@@ -4,6 +4,9 @@ from jaspyx.visitor import BaseVisitor
 class Return(BaseVisitor):
     def visit_Return(self, node):
         self.indent()
-        self.output('return ')
-        self.visit(node.value)
+        if node.value is not None:
+            self.output('return ')
+            self.visit(node.value)
+        else:
+            self.output('return')
         self.finish()
